7097dffc1404e91e0c0c00316afe47ff1fe04161
franciscovargas <vargfran@gmail.com>
2016-02-11 20:51:25 +0000

    fail

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import math
 import numpy
 import numpy as np
 from collections import deque
+import collections
 
 class Planner(object):
 	def __init__(self, world, simulator):
@@ -9,7 +10,8 @@ class Planner(object):
 		self.sim = simulator
 
 	def run(self):
-		costs = 
+		cost = self.cost_matrix(self.world)
+		for i in range()
 
 
 	def closest_drone(self):
@@ -40,6 +42,34 @@ class Planner(object):
 				elif dist > max_dist:
 					max_dist = dist
 			order_times[order] = max_dist
+
+
+
+	@staticmethod
+	def cost_matrix(world):
+		orders = self.world.orders
+		drones = [d from d in self.world.drones if d.dt == 0]
+		#  filter warehouses for products
+		warehouses = [w for w in self.world.warehouses]
+		cost_mat =[[]*len(drones)]*len(orders)
+		
+
+
+		for i, ordr in enumerate(orders):
+			for j, drone in enumerate(drones):
+				components = collections.Counter(ordr).getitems()
+				route = list()
+				ware = warehouses[0]
+				tot = float('inf')
+				while len(components)> 0:
+					ow = dist(ordr, warehouse)
+					dw = dist(drone, warehouse)
+					if tot > ow + dw:
+						ware = warehouse
+					
+				# for warehouse in warehouses[1:]:
+				cost_mat[i][j] = ware
+		return cost_mat
 
 	def cost(self, order, drone):
 
